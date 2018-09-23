b3d98a8874e939762baecc1d16fc78b49fa43899
Iaroslav Vorozhko <yaroslav@vorozhko.net>
2018-09-23 09:52:48 +0200

    added bastion host; renamed old eks tf configuration

---
+++ eks/bastion.tf
@@ -0,0 +1,32 @@
+
+resource "aws_instance" "bastion" {
+  ami                         = "ami-00035f41c82244dab"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  key_name                    = "eks-worker-ssh-key"
+  security_groups             = ["${aws_security_group.bastion-sg.name}"]
+  tags = {
+      Name = "bastion",
+      Terrafrom = "True"
+  }
+}
+
+
+resource "aws_security_group" "bastion-sg" {
+  name   = "bastion-security-group"
+  vpc_id = "${module.vpc.vpc_id}"
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = -1
+    from_port   = 0 
+    to_port     = 0 
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
